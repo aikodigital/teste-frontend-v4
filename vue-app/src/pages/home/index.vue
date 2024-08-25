cd03925df78c53ff07fe501dbc3799fdc4f1cb82
AdonaiSoaresM <adonaielite@gmail.com>
2024-08-24 23:12:46 -0300

    Feature: Leaflet Maps

--- vue-app/src/pages/home/index.vue
+++ vue-app/src/pages/home/index.vue
@@ -1,14 +1,17 @@
 <template>
     <section class="w-full h-full">
-      <Header></Header>
+      <Header/>
+      <Map/>
     </section>
   </template>
   <script lang="ts">
   import { defineComponent } from "vue";
   import Header from "./header.vue";
+  import Map from "./map.vue";
   export default defineComponent({
     components: {
       Header,
+      Map
     },
   });
   </script>
---
+++ vue-app/src/pages/home/map.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+      <div id="map" class="h-[600px] w-[600px]" />
+    </section>
+  </template>
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  import L from "leaflet";
+  export default defineComponent({
+    mounted(){
+      const map = L.map('map').setView([-19.126536, -45.947756], 13);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+          },
+  });
+  </script>
+  
