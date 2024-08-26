c2312cd7ef17779f195703e755d9f7064611e9a5
Leonardo Leal <leonardo.leal202@gmail.com>
2024-08-26 15:33:35 -0300

    feat: Add Aiko logo to navigation bar

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="border-b bg-white border-root-border">
+    <nav class="container mx-auto flex justify-center py-4">
+      <img src="@/assets/aiko.png" alt="Aiko Logo" class="w-24" />
+    </nav>
+  </div>
   <RouterView />
 </template>
