25f924e81b5330026cee44096d4973c7e6129fba
Matheusmdr <matheus.m.rocha@unesp.br>
2024-08-26 19:36:36 -0300

    fix: resolve hydration error in layout.tsx

--- forest-equipment-dashboard/src/app/layout.tsx
+++ forest-equipment-dashboard/src/app/layout.tsx
@@ -18,14 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
