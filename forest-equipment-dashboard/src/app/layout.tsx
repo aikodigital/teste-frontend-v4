ba5fba35babf678ac0d3b0313332a5eaa67e07fd
Matheusmdr <matheus.m.rocha@unesp.br>
2024-08-26 13:52:00 -0300

    feat: add dark mode support
    - Integrated next-themes for theme management
    - Added ThemeProvider for dark mode support
    - Created a ModeToggle component to switch between themes

--- forest-equipment-dashboard/src/app/layout.tsx
+++ forest-equipment-dashboard/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
