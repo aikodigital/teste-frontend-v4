0e64f8e82d390f6004f04b3472ff7b55f8a1c2c4
Ale Pereira <naitwa-alexandre@gmail.com>
2024-08-27 20:15:36 -0300

    first commit -m

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hello World!
+      </header>
+    </div>
+  );
+}
+
+export default App;
