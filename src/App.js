817598014fd3b80a53698f65269dfc3a5ec0fae9
Havyner Caetano <hrcgeo@gmail.com>
2024-08-27 15:47:02 -0300

    feat: Create basic map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-function App() {
-  return <div>oi</div>;
-}
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./index.css";
 
-export default App;
+import { Icon, divIcon, point } from "leaflet";
+
+// create custom icon
+const customIcon = new Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+  iconSize: [38, 38], // size of the icon
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function (cluster) {
+  return new divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true),
+  });
+};
+
+// markers
+const markers = [
+  {
+    geocode: [-19.126536, -45.947756],
+    popUp: "Hello, I am pop up 1",
+  },
+  {
+    geocode: [-19.167338, -46.00347],
+    popUp: "Hello, I am pop up 2",
+  },
+  {
+    geocode: [-19.192595, -46.061072],
+    popUp: "Hello, I am pop up 3",
+  },
+];
+
+export default function App() {
+  return (
+    <MapContainer center={[-19.126536, -45.947756]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map((marker) => (
+        <Marker position={marker.geocode} icon={customIcon}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
