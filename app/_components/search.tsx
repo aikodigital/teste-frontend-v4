7fa9a437c2d0382a1b170159bd976b49e7f88213
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-08-27 20:49:23 -0300

    feat: Add Search component and UI Input component

---
+++ app/_components/search.tsx
@@ -0,0 +1,9 @@
+import { Input } from "./ui/input";
+
+const Search = () => {
+    return ( 
+        <Input placeholder="Buscar equipamento" className="rounded-full bg-muted w-full" />
+     );
+}
+ 
+export default Search;
