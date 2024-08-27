4386a22666216ef7b7436401199c83fc4575a944
Dominique Campitelli <dominiquecamp@outlook.com>
2024-08-27 19:15:33 -0300

    Routes with react-router-dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
-export default function App() {
-  return (
-    <>
-      <h1>Aiko</h1>
-    </>
-  );
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+export function App() {
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      Aiko
+    </div>
+  );
+}
