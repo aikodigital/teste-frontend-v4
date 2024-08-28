6232e4409fac514fd96d7a4969d182a0c68e56dd
angelogatz <angeloagatz@gmail.com>
2024-08-28 02:54:57 -0300

    feat:  - adding new icon;  - adding lat lng center finder function;  - adding icon hover function;

--- src/components/modules/Map/Map.tsx
+++ src/components/modules/Map/Map.tsx
@@ -1,43 +1,137 @@
-import React from 'react';
-import { GoogleMap, LoadScript, GoogleMapProps } from '@react-google-maps/api';
+import React, { useEffect, useMemo, useState } from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
-import './Map.scss'
+import equipmentPositionHistory from '../../../data/equipmentPositionHistory.json'
 
-// Definindo o tipo das propriedades do componente
-interface MapProps {}
+import icon from '../../../assets/icons/Truck_2.png'
 
-const containerStyle: React.CSSProperties = {
-  width: '70%',
+import './Map.scss';
+
+const MAP_STYLE: React.CSSProperties = {
+  width: '100%',
   height: '500px',
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+// const CENTER_COORDINATES: { lat: number; lng: number } = {
+//   lat: -3.745,
+//   lng: -38.523,
+// };
 
-const Map: React.FC<MapProps> = () => {
-  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+// const MARKERS: { lat: number; lng: number; title: string; iconColor: string }[] = [
+//   { lat: -3.745, lng: -38.523, title: 'Marker 1', iconColor: 'red' },
+//   { lat: -3.735, lng: -38.513, title: 'Marker 2', iconColor: 'blue' },
+//   { lat: -3.725, lng: -38.503, title: 'Marker 3', iconColor: 'green' },
+// ];
 
-  if (!apiKey) {
-    console.warn('API key is missing.')
+const API_KEY: string | undefined = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+const Map: React.FC = () => {
+  const [centerCoords, setCenterCoords] = useState<{ lat: number; lng: number } | undefined>(undefined);
+
+  const [isHovered, setIsHovered] = useState<string>('');
+
+  const { isLoaded }: { isLoaded: boolean } = useJsApiLoader({
+    id: "google-maps-script",
+    googleMapsApiKey: API_KEY as string,
+  });
+
+  function averageGeolocation(): { latitude: number; longitude: number } {
+    const coords: { lat: number; lon: number }[] = equipmentPositionHistory.map(history => {
+      const lastPosition = history.positions[history.positions.length - 1];
+      return { lat: lastPosition.lat, lon: lastPosition.lon };
+    });
+
+    console.log({ coords });
+
+    if (coords.length === 1) {
+      return {
+        latitude: coords[0].lat,
+        longitude: coords[0].lon
+      };
+    }
+
+    let x: number = 0.0;
+    let y: number = 0.0;
+    let z: number = 0.0;
+
+    for (let coord of coords) {
+      let latitude: number = coord.lat * Math.PI / 180;
+      let longitude: number = coord.lon * Math.PI / 180;
+
+      x += Math.cos(latitude) * Math.cos(longitude);
+      y += Math.cos(latitude) * Math.sin(longitude);
+      z += Math.sin(latitude);
+    }
+
+    let total: number = coords.length;
+
+    x = x / total;
+    y = y / total;
+    z = z / total;
+
+    let centralLongitude: number = Math.atan2(y, x);
+    let centralSquareRoot: number = Math.sqrt(x * x + y * y);
+    let centralLatitude: number = Math.atan2(z, centralSquareRoot);
+
+    const result = {
+      latitude: centralLatitude * 180 / Math.PI,
+      longitude: centralLongitude * 180 / Math.PI
+    };
+
+    setCenterCoords({
+      lat: result.latitude,
+      lng: result.longitude
+    });
+
+    return result;
+  }
+
+  useEffect(() => {
+    if(!equipmentPositionHistory) return alert('coords is missing')
+    averageGeolocation()
+  }, [])
+
+  if(!API_KEY) {
+    alert('API key is missing.');
     return null;
   }
 
   return (
     <div className='map-container'>
-      <LoadScript
-        googleMapsApiKey={apiKey}
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          key={apiKey}
-        >
-          { /* Adicione seus marcadores ou outros componentes do mapa aqui */ }
-        </GoogleMap>
-      </LoadScript>
+      {isLoaded 
+        ? (
+          <GoogleMap
+            mapContainerStyle={MAP_STYLE}
+            center={centerCoords}
+            zoom={10}
+          >
+            <div className='marker-container'>
+              {equipmentPositionHistory.map((history, index) => {
+                const lastPosition = history.positions[history.positions.length - 1];
+                const id = history.equipmentId
+                console.log({history})
+                const date = new Date(lastPosition.date)
+                return (
+                  <Marker
+                    key={index}
+                    position={{ lat: lastPosition.lat, lng: lastPosition.lon }}
+                    onMouseOver={() => setIsHovered(id)}
+                    onMouseOut={() => setIsHovered('')}
+                    label={date.toLocaleDateString('pt-BR')}
+                    icon={{
+                      url: icon,
+                      scaledSize: new google.maps.Size((isHovered == id) ? 60 : 50, (isHovered == id) ? 60 : 50),
+                      anchor: new google.maps.Point(25, 25),
+                    }}
+                  />
+                )
+              })}
+            </div>
+          </GoogleMap>
+          ):(
+          <></>
+      )}
+      
     </div>
   );
 };
