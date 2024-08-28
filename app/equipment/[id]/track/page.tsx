c9ffaf9141ca9d2538d5c8e07dc33204daa11774
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-08-28 16:47:40 -0300

    feat: Remove menu component from layout and update page with equipment data

---
+++ app/equipment/[id]/track/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import equipmentDetails from "@/app/data/equipment.json";
+import equipmentModels from "@/app/data/equipmentModel.json";
+import equipmentStateHistory from "@/app/data/equipmentStateHistory.json";
+
+const TrackPage = () => {
+  const [equipment, setEquipment] = useState<
+    (typeof equipmentDetails)[0] | null
+  >(null);
+  const [productivity, setProductivity] = useState<number>(0);
+  const [earnings, setEarnings] = useState<number>(0);
+  const [positionHistory, setPositionHistory] = useState(undefined);
+  const [stateHistory, setStateHistory] = useState(null);
+
+  const router = useRouter();
+  const equipmentId = "";
+
+  useEffect(() => {
+    const equipmentFinded = equipmentDetails.find(
+      (row) => row.id === equipmentId
+    );
+
+    if (equipmentFinded) {
+      setEquipment(equipmentFinded);
+
+      const model = equipmentModels.find(
+        (model) => model.id === equipmentFinded.equipmentModelId
+      );
+      const stateHistory = equipmentStateHistory.find(
+        (history) => history.equipmentId === equipmentId
+      );
+    }
+  }, [equipmentId]);
+
+  return (
+    <div>
+      <h1>Track Page</h1>
+      <p>{equipmentId}</p>
+      {/* Renderize o histórico de posição e estado aqui */}
+    </div>
+  );
+};
+
+export default TrackPage;
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import equipment from "@/app/data/equipment.json";
 import equipmentModel from "@/app/data/equipmentModel.json";
 import equipmentPositionHistory from "@/app/data/equipmentPositionHistory.json";
 import equipmentStateHistory from "@/app/data/equipmentStateHistory.json";
+import Menu from "./_components/menu";
 
 const DynamicMap = dynamic(() => import("./_components/map"), {
   ssr: false,
@@ -13,25 +14,30 @@ const DynamicMap = dynamic(() => import("./_components/map"), {
 
 const Home = () => {
   return (
-    <main className="w-[100vw] h-[100vh]">
-      <div className="fixed z-50 left-3 top-3 w-[100px] h-[50px]">
-        <Image
-          src="/aiko.png"
-          alt="Logo"
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          style={{ width: "auto", height: "auto" }}
-          width={100}
-          height={50}
-        />
-      </div>
-      <div className="z-10">
-        <DynamicMap
-          equipment={equipment}
-          equipmentPositionHistory={equipmentPositionHistory}
-          equipmentModel={equipmentModel}
-          equipmentStateHistory={equipmentStateHistory}
-        />
+    <main className="flex justify-center items-center h-screen">
+      <section className="h-[100vh] w-[100vw]">
+        <div className="fixed z-50 left-3 top-3 w-[100px] h-[50px]">
+          <Image
+            src="/aiko.png"
+            alt="Logo"
+            priority
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ width: "auto", height: "auto" }}
+            width={100}
+            height={50}
+          />
+        </div>
+        <div className="z-10">
+          <DynamicMap
+            equipment={equipment}
+            equipmentPositionHistory={equipmentPositionHistory}
+            equipmentModel={equipmentModel}
+            equipmentStateHistory={equipmentStateHistory}
+          />
+        </div>
+      </section>
+      <div className="z-50 fixed bottom-3 left-auto right-auto">
+        <Menu />
       </div>
     </main>
   );
