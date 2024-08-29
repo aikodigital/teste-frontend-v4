25ca7e5e1a327593dbf78c3075b38dbb71c47c2d
Raul Cabrera <rcabr31@gmail.com>
2024-08-29 09:26:15 -0300

    fix: add map components file update

---
+++ components/Equipment/Map.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface MapMarker {
+  key: string
+  name: string
+  model: string
+  state: string
+  lat: number
+  lon: number
+  date: string
+  color: string
+}
+
+interface MapProps {
+  markers: MapMarker[]
+  zoom: number
+  hasPath?: boolean
+}
+
+const props = defineProps<MapProps>()
+
+const mapPath = props.markers.map(marker => ({ lat: marker.lat, lng: marker.lon }))
+</script>
+
+<template>
+  <div id="leaflet-map-container">
+    <LMap :zoom="props.zoom" :center="[-19.16, -45.95]" :useGlobalLeaflet="false">
+      <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layerType="base" />
+      <LCircleMarker
+        v-for="marker in props.markers"
+        :key="marker.key"
+        :latLng="[marker.lat, marker.lon]"
+        :color="formatFromStringToHashColorHex(marker.model)"
+        :radius="8"
+      >
+        <LTooltip>
+          <slot name="tooltip" :marker="marker" />
+        </LTooltip>
+      </LCircleMarker>
+      <LPolyline
+        v-if="props.hasPath"
+        :latLngs="mapPath"
+        :color="'gray'"
+        :opacity="0.3"
+      />
+    </LMap>
+  </div>
+</template>
+
+<style scoped>
+#leaflet-map-container {
+  height: 60vh;
+  width: 100%;
+}
+</style>
