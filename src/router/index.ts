67f77499e035965eda50cddcb9d2522bd031e3d2
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-28 22:17:23 -0300

    ♻️ refact: change routes structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import routes from '~/router/routes'
+
+const history = createWebHistory()
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('~/views/Home.vue'),
-    },
-  ],
+  history,
+  routes,
 })
 
 export default router
