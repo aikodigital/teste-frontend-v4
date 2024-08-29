3484eed24b066d82399c8f74bf7b4c5061f28c96
Raul Cabrera <rcabr31@gmail.com>
2024-08-28 21:58:51 -0300

    fix: diff

---
+++ pages/equipments/[id]/map.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const router = useRouter()
+
+const route = useRoute()
+const equipmentId = route.params.id.toString()
+
+const { getEquipment } = useEquipments()
+const equipment = getEquipment(equipmentId)
+
+const mapMarkers = computed(() => {
+  if (!equipment || !equipment.positions.length) return []
+
+  const recentStateDate = equipment.stateDates.length ?
+    equipment.stateDates.reduce((recentStateDate, stateDate) => {
+      return recentStateDate.date > stateDate.date ? recentStateDate : stateDate
+    }) : null
+
+  const sortedEquipmentPositions = equipment.positions.sort((positionA, positionB) => {
+    return positionA.date.localeCompare(positionB.date)
+  })
+
+  return sortedEquipmentPositions.map(position => ({
+    key: equipment.id,
+    name: equipment.name,
+    model: equipment.model?.name ?? 'Não informado',
+    state: recentStateDate?.state?.name ?? 'Sem histórico',
+    lat: position.lat,
+    lon: position.lon,
+    color: recentStateDate?.state?.color ?? 'inherit'
+  }))
+})
+</script>
+
+<template>
+  <div id="equipment-map-container">
+    <div class="map-info">
+      <div class="info-title">{{ equipment?.name }} ({{ equipment?.model?.name }})</div>
+      <div class="info-actions">
+        <div class="back-button" @click="router.back">
+          <FontAwesomeIcon :icon="['fas', 'arrow-left']" />
+          Voltar
+        </div>
+      </div>
+    </div>
+    <AppMap
+      :markers="mapMarkers"
+      :markerRadius="5"
+      :zoom="11"
+      :hasPath="true"
+    />
+  </div>
+</template>
+
+<style scoped>
+#equipment-map-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 12px;
+  align-items: stretch;
+
+  .map-info {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: var(--container-color);
+    border-radius: 4px;
+    gap: 32px;
+    padding: 16px 32px;
+
+    .info-title {
+      font-size: 24px;
+      font-weight: bold;
+    }
+
+    .info-actions {
+      align-self: center;
+      justify-self: end;
+
+      .back-button {
+        font-weight: bold;
+        color: var(--primary-color);
+
+        &:hover {
+          color: var(--secondary-color);
+        }
+      }
+    }
+  }
+}
+</style>
