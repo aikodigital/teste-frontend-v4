e1fd946a027fa5c0a1b29607723d965e1cf0210d
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-28 22:16:06 -0300

    ✨ feat: create icon component using iconify dependency

---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Icon as IconifyIcon } from '@iconify/vue'
+
+interface Props {
+  icon: string
+  width?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <IconifyIcon
+    :icon
+    :width
+  />
+</template>
