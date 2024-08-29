030a645a9b65c27c15560b7157206273f48ed818
Havyner Caetano <hrcgeo@gmail.com>
2024-08-29 11:36:38 -0300

    feat: Add icon to each position marker

--- src/components/PositionMarker.jsx
+++ src/components/PositionMarker.jsx
@@ -1,6 +1,5 @@
-import { Stack, Text } from "@mantine/core";
+import { Box, Stack, Text } from "@mantine/core";
 import L from "leaflet";
-import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
 import ReactDOMServer from "react-dom/server";
 import { useMap } from "react-leaflet";
@@ -8,17 +7,31 @@ import { useMap } from "react-leaflet";
 const PositionMarker = ({ data }) => {
   const map = useMap();
 
+  const renderComponentToHtml = (component) => {
+    return ReactDOMServer.renderToStaticMarkup(component);
+  };
+
   const positionIcon = (positionNumber) => {
+    const content = renderComponentToHtml(
+      <Stack spacing={0}>
+        <Box sx={{ marginBottom: -12 }}>
+          <svg width="24" height="32" viewBox="0 0 24 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M12 3.5C8.44 3.5 5.5 6.44 5.5 10c0 5.5 6.5 14 6.5 14s6.5-8.5 6.5-14c0-3.56-2.94-6.5-6.5-6.5zM12 18a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"
+              fill="#000"
+            />
+          </svg>
+        </Box>
+        <Text>{positionNumber}</Text>
+      </Stack>
+    );
+
     return L.divIcon({
       className: "position-icon",
-      html: `<div>${positionNumber}</div>`,
+      html: content,
       iconSize: [20, 20],
       iconAnchor: [10, 10],
     });
-  };
-
-  const renderComponentToHtml = (component) => {
-    return ReactDOMServer.renderToStaticMarkup(component);
   };
 
   useEffect(() => {
@@ -39,6 +52,7 @@ const PositionMarker = ({ data }) => {
         marker.bindPopup(content).openPopup();
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data.positions, map]);
 };
 
