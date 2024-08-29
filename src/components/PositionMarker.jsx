3c4b5b74e719de654a4fe2741c22841eb7547513
Havyner Caetano <hrcgeo@gmail.com>
2024-08-29 11:14:28 -0300

    feat: Add coordinates to each trajectory marker

--- src/components/PositionMarker.jsx
+++ src/components/PositionMarker.jsx
@@ -1,6 +1,8 @@
+import { Stack, Text } from "@mantine/core";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
+import ReactDOMServer from "react-dom/server";
 import { useMap } from "react-leaflet";
 
 const PositionMarker = ({ data }) => {
@@ -15,11 +17,26 @@ const PositionMarker = ({ data }) => {
     });
   };
 
+  const renderComponentToHtml = (component) => {
+    return ReactDOMServer.renderToStaticMarkup(component);
+  };
+
   useEffect(() => {
     if (data.positions?.length > 0) {
       data.positions?.forEach((position, index) => {
+        const content = renderComponentToHtml(
+          <Stack spacing={8}>
+            <Text weight={700} ta="center">
+              Coordenadas
+            </Text>
+            <Text>
+              Latitude: {position.lat}, longitude: {position.lon}
+            </Text>
+          </Stack>
+        );
+
         const marker = L.marker([position.lat, position.lon], { icon: positionIcon(index + 1) }).addTo(map);
-        marker.bindPopup("This is a custom Leaflet marker!").openPopup();
+        marker.bindPopup(content).openPopup();
       });
     }
   }, [data.positions, map]);
