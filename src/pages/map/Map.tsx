da23a0c60203739cf01062d3965ba54e5daa194a
diegomota2374 <diegomota2374@gmail.com>
2024-08-30 00:59:08 -0300

    configue firt position in map, page map

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,47 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
-import './Map.css';
-import { EquipmentPositionHistory } from '../../models/EquipmentPositionHistory';
-
-
+import React, { useEffect, useState } from "react";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import "./Map.css";
+import { EquipmentPositionHistory } from "../../models/EquipmentPositionHistory";
 
 const MapComponent = () => {
+  const [equipmentHistory, setEquipmentHistory] =
+    useState<EquipmentPositionHistory>({
+      equipmentId: "",
+      positions: [],
+    });
 
-  const [equipmentHistory, setEquipmentHistory ] = useState([]);
-  const position = {lat: 53.54992, lng: 10.00678};
-
-  
-  
-  
-  
-  useEffect(()=> {
+  useEffect(() => {
     getEquipmentPositionHistory();
-  }, [])
-
+  }, []);
 
   const getEquipmentPositionHistory = async () => {
     try {
-      const resp : any =  await fetch('data/equipmentPositionHistory.json');
+      const resp: any = await fetch("data/equipmentPositionHistory.json");
       const data = await resp.json();
-      setEquipmentHistory(data);
-    }catch(e){
-      console.log(e)
+      setEquipmentHistory(data[0]);
+    } catch (e) {
+      console.log(e);
     }
-   
+  };
+
+  if (!equipmentHistory.positions.length) {
+    return null;
   }
-  
+
+  const firstPosition = {
+    lat: equipmentHistory.positions[0].lat,
+    lng: equipmentHistory.positions[0].lon,
+  };
+
   return (
-    <div className='map-container'>
-       <APIProvider apiKey={process.env.REACT_APP_MAP_KEY || ''}>
-      <Map defaultCenter={position} defaultZoom={10}>
-        <Marker position={position} />
-      </Map>
-    </APIProvider>
+    <div className="map-container">
+      <APIProvider apiKey={process.env.REACT_APP_MAP_KEY || ""}>
+        <Map defaultCenter={firstPosition} defaultZoom={10}>
+          {equipmentHistory.positions.map((position, index) => (
+            <Marker
+              key={index}
+              position={{ lat: position.lat, lng: position.lon }}
+            />
+          ))}
+        </Map>
+      </APIProvider>
     </div>
-   
   );
-}
-
-
+};
 
 export default MapComponent;
