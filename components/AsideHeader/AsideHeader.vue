6384f4173dd567bea0bd6816b601818b1d4e9529
Nicolas Walcker <walcker_@outlook.com>
2024-08-30 00:05:30 -0300

    feat: create aside components

---
+++ components/AsideHeader/AsideHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-auto w-full justify-between">
+    <span class="flex items-center justify-start gap-0.5 font-black"><Icon name="material-symbols:radar" size="24" /> Equipment Tracker</span>
+    <Popover>
+      <PopoverTrigger as-child>
+        <Button variant="ghost" size="icon">
+          <Icon name="lucide:info" size="20" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <Legend />
+      </PopoverContent>
+    </Popover>
+  </div>
+</template>
