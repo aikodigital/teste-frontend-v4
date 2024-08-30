acc08d0703f68edc0f29e07fb8b4bcf518d21669
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-29 21:04:05 -0300

    ♻️ refact: adjust equipment filter function

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,8 +1,10 @@
 import states from '../../data/equipmentState.json'
 import equipments from '../../data/equipment.json'
+import statesHistory from '../../data/equipmentStateHistory.json'
+import type { EquipmentStatus } from '~/models/Equipment'
 
-export const formatDate = (date: string) => {
-  const isValidDate = !isNaN(Date.parse(date))
+export function formatDate(date: string) {
+  const isValidDate = !Number.isNaN(Date.parse(date))
   if (!isValidDate) {
     return 'Invalid Date'
   }
@@ -13,11 +15,35 @@ export const formatDate = (date: string) => {
 }
 
 export function returnEquipmentBasedOnId(equipmentId: string) {
-  const equipment = equipments.find(equipment => equipment.id === equipmentId);
+  const equipment = equipments.find(equipment => equipment.id === equipmentId)
 
   return equipment ? equipment.name : 'Equipamento não identificado'
 }
 
 export function returnStateBasedOnId(id: string) {
-  return states.filter((state) => state.id === id)
+  return states.filter(state => state.id === id)
+}
+
+export function returnEquipmentStateBasedOnDate(equipmentId: string, date: string): EquipmentStatus | undefined {
+  const stateHistory = statesHistory.find(stateHistory => stateHistory.equipmentId === equipmentId)
+
+  if (stateHistory) {
+    const targetDate = new Date(date)
+
+    const matchedState = stateHistory.states
+      .map(state => ({
+        ...state,
+        date: new Date(state.date),
+      }))
+      .filter(state => state.date <= targetDate)
+      .sort((a, b) => b.date.getTime() - a.date.getTime())
+      .shift()
+
+    if (matchedState) {
+      const equipmentState = returnStateBasedOnId(matchedState.equipmentStateId)
+      return equipmentState[0]
+    }
+  }
+
+  return undefined
 }
