4fc262d6560d64e9cc50587a5d151535e52f78f0
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-29 21:04:15 -0300

    ✨ feat: improve home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,33 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import Map from '~/components/Map.vue'
-import SelectButton from '~/components/form/SelectButton.vue';
-import type { Equipment, EquipmentStatus } from '~/models/Equipment';
+import { ref } from 'vue'
 import equipments from '../../data/equipment.json'
 import equipmentStates from '../../data/equipmentState.json'
+import Map from '~/components/Map.vue'
+import SelectButton from '~/components/form/SelectButton.vue'
+import type { Equipment, EquipmentStatus } from '~/models/Equipment'
+import { notify } from '~/utils/notify'
 
 const equipmentFiltered = ref()
 const statusFiltered = ref()
 
 function equipmentOptions(equipments: Array<Equipment>) {
-  return equipments.map(({ equipmentModelId, ...rest }) => rest);
+  return equipments.map(({ equipmentModelId, ...rest }) => rest)
 }
 
 function equipmentStatus(equipmentStates: Array<EquipmentStatus>) {
-  return equipmentStates.map(({ color, ...rest }) => rest);
+  return equipmentStates.map(({ color, ...rest }) => rest)
 }
 
 function clearFilters() {
+  if (!equipmentFiltered.value && !statusFiltered.value) {
+    notify('Não há filtros para serem limpos 🤨', 'warning')
+    return
+  }
+
   equipmentFiltered.value = undefined
   statusFiltered.value = undefined
+
+  notify('Os filtros foram limpos! 😄', 'success')
 }
 </script>
 
@@ -29,13 +37,13 @@ function clearFilters() {
       <img
         class="object-scale-down aspect-w-3 aspect-h-2 w-[100px]"
         src="/src/assets/aiko.png"
-      />
+      >
     </div>
     <div class="flex flex-col sm:flex-row h-[40rem] w-[90%] mx-auto mb-[5rem] gap-2">
       <div class="grid grid-cols-2 items-start justify-start border border-neutral rounded-lg bg-base-200 w-full sm:w-1/3">
         <div class="flex w-full h-[100px] border-b border-neutral col-span-2 p-8 justify-between">
           <h1
-          class="text-xl font-bold"
+            class="text-xl font-bold"
           >
             Filtros
           </h1>
@@ -47,7 +55,7 @@ function clearFilters() {
             Limpar filtros
           </button>
         </div>
-        <div class="flex w-full border-b border-neutral col-span-2 p-7 gap-x-2">
+        <div class="flex flex-col w-full border-b border-neutral col-span-2 p-7 gap-2">
           <SelectButton
             v-model="equipmentFiltered"
             :options="equipmentOptions(equipments)"
@@ -63,7 +71,11 @@ function clearFilters() {
       <div class="h-full w-full sm:w-2/3">
         <Map
           :equipment-filtered="equipmentFiltered"
+          :status-filtered="statusFiltered"
           class="rounded-lg"
+          @change-selected-equipment="(newEquipmentId: string) => {
+            equipmentFiltered = newEquipmentId
+          }"
         />
       </div>
     </div>
