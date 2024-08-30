aea2d1eec8359ea636fae1888c66d8c2e2f1334e
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-08-30 02:17:49 -0300

    actions

--- src/app/services/actions.ts
+++ src/app/services/actions.ts
@@ -16,35 +16,35 @@ export async function getEquipment() {
 
 export async function getEquipmentModel() {
     const modelResponse = await getData("/data/equipmentModel.json");
-    const modelData = modelResponse.json();
+    const modelData = modelResponse;
 
     return modelData;
 }
 
 export async function getEquipmentState() {
     const statesResponse = await getData("data/equipmentState.json");
-    const statesData = statesResponse.json();
+    const statesData = statesResponse;
 
     return statesData;
 }
 
 export async function getEquipmentStateHistory() {
     const stateHistoryResponse = await getData("/data/equipmentStateHistory.json");
-    const stateHistoryData = stateHistoryResponse.json();
+    const stateHistoryData = stateHistoryResponse;
 
     return stateHistoryData;
 }
 
 export async function getEquipmentPositionHistory() {
     const positionHistoryResponse = await getData("/data/equipmentPositionHistory.json");
-    const positionHistoryData = positionHistoryResponse.json();
+    const positionHistoryData = positionHistoryResponse;
 
     return positionHistoryData;
 }
 
-const getData = async (url: string, options = {}) => {
+const getData = async (url: string) => {
     try {
-      const response = await axios.get(url, options,);
+      const response = await axios.get(url);
       return response.data;
     } catch (error) {
       throw new Error("Could not get data");
