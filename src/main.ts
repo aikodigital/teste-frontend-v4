07403d769b6d95beaa9b5e0118d204b966cf4843
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-29 21:01:59 -0300

    ✨ feat: add vue toastification to main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import '~/assets/styles/styles.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+import { ToastOptions } from '~/config/toast'
 
 import App from '~/App.vue'
 import router from '~/router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(Toast, ToastOptions)
+  .mount('#app')
