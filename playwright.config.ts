a1b30e65319c383704d1c874d28cc796249c350a
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-29 21:39:20 -0300

    ✨ feat: add e2e tests for home page

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,12 +57,6 @@ export default defineConfig({
         ...devices['Desktop Firefox'],
       },
     },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
 
     /* Test against mobile viewports. */
     // {
