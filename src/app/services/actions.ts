fb5af82191269f693a8b9bcbd470c96f28c28cf2
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-08-30 02:11:24 -0300

    actions

--- src/app/services/actions.ts
+++ src/app/services/actions.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Equipment } from "@/types/Equipment";
 import { EquipmentState } from "@/types/EquipmentState";
 
@@ -7,36 +8,45 @@ export function getStateNameById(stateId: string, states: EquipmentState[]): str
 }
 
 export async function getEquipment() {
-    const equipmentResponse = await fetch("/data/equipment.json");
-    const equipmentData: Equipment[] = await equipmentResponse.json();
+    const equipmentResponse = await getData("/data/equipment.json");
+    const equipmentData: Equipment[] = await equipmentResponse;
 
     return equipmentData;
 }
 
 export async function getEquipmentModel() {
-    const modelResponse = await fetch("/data/equipmentModel.json");
+    const modelResponse = await getData("/data/equipmentModel.json");
     const modelData = modelResponse.json();
 
     return modelData;
 }
 
 export async function getEquipmentState() {
-    const statesResponse = await fetch("data/equipmentState.json");
+    const statesResponse = await getData("data/equipmentState.json");
     const statesData = statesResponse.json();
 
     return statesData;
 }
 
 export async function getEquipmentStateHistory() {
-    const stateHistoryResponse = await fetch("/data/equipmentStateHistory.json");
+    const stateHistoryResponse = await getData("/data/equipmentStateHistory.json");
     const stateHistoryData = stateHistoryResponse.json();
 
     return stateHistoryData;
 }
 
 export async function getEquipmentPositionHistory() {
-    const positionHistoryResponse = await fetch("/data/equipmentPositionHistory.json");
+    const positionHistoryResponse = await getData("/data/equipmentPositionHistory.json");
     const positionHistoryData = positionHistoryResponse.json();
 
     return positionHistoryData;
 }
+
+const getData = async (url: string, options = {}) => {
+    try {
+      const response = await axios.get(url, options,);
+      return response.data;
+    } catch (error) {
+      throw new Error("Could not get data");
+    }
+  };
