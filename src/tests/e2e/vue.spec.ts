2980258439f4cdaa1614f21dcba5f933d293bd14
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-29 21:40:03 -0300

    ✨ feat: add new e2e tests

--- src/tests/e2e/vue.spec.ts
+++ src/tests/e2e/vue.spec.ts
@@ -1,8 +1,23 @@
-import { expect, test } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!')
-})
+test('deve renderizar a página corretamente', async ({ page }) => {
+  await page.goto('/');
+
+  const logo = page.locator('img[alt="Logo Aiko"]');
+  await expect(logo).toBeVisible();
+
+  const title = page.locator('h1:text("Filtros")');
+  await expect(title).toBeVisible();
+
+  const clearFiltersButton = page.locator('button:text("Limpar filtros")');
+  await expect(clearFiltersButton).toBeVisible();
+
+  const equipmentSelectButton = page.locator('text=Filtrar por equipamento:');
+  await expect(equipmentSelectButton).toBeVisible();
+
+  const statusSelectButton = page.locator('text=Filtrar por status:');
+  await expect(statusSelectButton).toBeVisible();
+
+  const legend = page.locator('p:text("Legenda:")');
+  await expect(legend).toBeVisible();
+});
