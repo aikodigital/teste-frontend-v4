c8a5817f399913d22f15ba94994f0e46d5e7c4f8
gusleaooliveira <gus.leaono@gmail.com>
2024-09-02 02:21:03 -0300

    fix build

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,10 @@
 import { MantineProvider } from '@mantine/core';
- 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import dynamic from 'next/dynamic';
 
 const NoSSRComponent = dynamic(() => import('./screens/home'), { ssr: false });
 
 
-export default function Home() {
-  const queryClient = new QueryClient();
+export default function Home() { 
   return (
       <MantineProvider forceColorScheme="dark"> 
         <NoSSRComponent />
