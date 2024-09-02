5fd5cf8162061f2ab925a597fc11c9527b2939f5
gusleaooliveira <gus.leaono@gmail.com>
2024-09-02 02:39:09 -0300

    fix build

--- frontend/hooks/api.ts
+++ frontend/hooks/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log('API Base URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
-
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
