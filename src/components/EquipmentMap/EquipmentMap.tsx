c1305e6c99eeb86d8975fd2e562d80ccbff41309
Samuel Condack <samuelcondackxz@gmail.com>
2024-09-12 20:31:40 -0300

    add interfaces

---
+++ src/components/EquipmentMap/EquipmentMap.tsx
@@ -0,0 +1,20 @@
+
+
+interface Position {
+  id: string;
+  lat: number;
+  lon: number;
+}
+
+interface State {
+  id: string;
+  name: string;
+  color: string;
+}
+
+interface Model {
+  id: string;
+  name: string;
+  hourlyEarnings: any[];
+}
+
