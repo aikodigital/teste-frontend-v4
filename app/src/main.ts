fd7db97de0e22624d898688114b7fe99367227f6
Saviovd <savio.pinheiroal@gmail.com>
2024-09-13 16:27:50 -0400

    delete: cleaning application

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
+import App from './views/App.vue';
 import './assets/tailwind.css';
 
 const app = createApp(App);
---
+++ app/src/views/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Hello world</div>
+</template>
