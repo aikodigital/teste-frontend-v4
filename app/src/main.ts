2d3875eca4a0f6dccbd1bb0f29a91e1a8b3007bc
Saviovd <savio.pinheiroal@gmail.com>
2024-09-13 17:19:07 -0400

    add: style configuration

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './views/App.vue';
+import 'tailwindcss/tailwind.css';
 import './assets/tailwind.css';
 
 const app = createApp(App);
