86097752ddd47b981492f7497fc062f3fb42c035
Samuel Condack <samuelcondackxz@gmail.com>
2024-09-12 21:27:22 -0300

    add equipment types

--- src/types/Equipment.ts
+++ src/types/Equipment.ts
@@ -1,17 +1,46 @@
 export interface Equipment {
-    id: string;
-    name: string;
-    equipmentModelId: string;
-  }
-  
-  export interface Position {
-    lat: number;
-    lon: number;
-    date: string;
-  }
-  
-  export interface StateHistory {
-    date: string;
-    equipmentStateId: string;
-  }
-  
+  id: string;
+  name: string;
+  equipmentModelId: string;
+}
+
+export interface Position {
+  lat: number;
+  lon: number;
+  date: string;
+}
+
+export interface StateHistory {
+  date: string;
+  equipmentStateId: string;
+}
+
+export interface Position {
+  id: string;
+  lat: number;
+  lon: number;
+  date: string;
+}
+
+export interface State {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface Model {
+  id: string;
+  name: string;
+  hourlyEarnings: any[];
+}
+
+export interface Equipment {
+  id: string;
+  name: string;
+  equipmentModelId: string;
+}
+
+export interface EquipmentHistory {
+  date: string;
+  equipmentStateId: string;
+}
