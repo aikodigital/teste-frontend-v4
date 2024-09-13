585bdd3e947e1e5cf8c248f283ec202997500b2c
Larissa Fernandes <larissa-cdz@hotmail.com>
2024-09-13 14:08:28 -0300

    install vuetify

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,3 @@ import { RouterView } from 'vue-router'
 
   <RouterView />
 </template>
-
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
+
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
