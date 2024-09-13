5e9b87331ebbb23ab8571882855ea559b2da5da2
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2024-09-13 11:54:59 -0300

    Finaliza o componente de mapa com as informacoes das maquinas

--- teste-frontend-aiko/src/components/LocateMachines/LocateMachines.tsx
+++ teste-frontend-aiko/src/components/LocateMachines/LocateMachines.tsx
@@ -12,7 +12,18 @@ interface MachineData {
   positions: Position[];
 }
 
-interface MachineInfo {
+interface StateData {
+  equipmentId: string;
+  states: { date: string; equipmentStateId: string }[];
+}
+
+interface StateInfo {
+  id: string;
+  name: string;
+  color: string;
+}
+
+interface ModelInfo {
   id: string;
   name: string;
   hourlyEarnings: { equipmentStateId: string; value: number }[];
@@ -23,37 +34,87 @@ interface ProcessedMachine {
   lat: number;
   lng: number;
   name: string;
+  stateName: string;
+  stateColor: string;
+  modelName: string;
 }
 
 export const LocateMachines = () => {
   const [machines, setMachines] = useState<ProcessedMachine[]>([]);
   const defaultCenter = [-19.126536, -45.947756];
-  const defaultZoom = 12;
+  const defaultZoom = 8;
 
   useEffect(() => {
     Promise.all([
       fetch("/data/equipmentPositionHistory.json").then((response) =>
         response.json()
       ),
+      fetch("/data/equipmentStateHistory.json").then((response) =>
+        response.json()
+      ),
+      fetch("/data/equipmentState.json").then((response) => response.json()),
       fetch("/data/equipmentModel.json").then((response) => response.json()),
-    ]).then(([positionsData, infoData]: [MachineData[], MachineInfo[]]) => {
-      const infoMap = new Map<number, string>();
-      infoData.forEach((info) => {
-        infoMap.set(parseInt(info.id), info.name); // Retornando para int
-      });
+      fetch("/data/equipment.json").then((response) => response.json()), 
+    ]).then(
+      ([
+        positionsData,
+        statesData,
+        stateInfoData,
+        modelInfoData,
+        equipmentData,
+      ]) => {
+        const stateInfoMap = stateInfoData.reduce((acc, state) => {
+          acc[state.id.toLowerCase()] = {
+            name: state.name,
+            color: state.color,
+          };
+          return acc;
+        }, {});
 
-      const processedMachines = positionsData.map((machine) => {
-        return {
-          id: machine.equipmentId,
-          lat: machine.positions[machine.positions.length - 1].lat,
-          lng: machine.positions[machine.positions.length - 1].lon,
-          name: infoMap.get(machine.equipmentId) || "Desconhecida", 
-        };
-      });
-      setMachines(processedMachines);
-    });
+        const modelInfoMap = modelInfoData.reduce((acc, model) => {
+          acc[model.id.toLowerCase()] = model.name;
+          return acc;
+        }, {});
+
+        const equipmentMap = equipmentData.reduce((acc, equipment) => {
+          acc[equipment.id.toLowerCase()] =
+            equipment.equipmentModelId.toLowerCase();
+          return acc;
+        }, {});
+
+        const processedMachines = positionsData.map((machine) => {
+          const machineIdLower = machine.equipmentId.toLowerCase();
+
+          const equipmentModelId = equipmentMap[machineIdLower];
+          const modelName =
+            modelInfoMap[equipmentModelId] || "Modelo Desconhecido";
+
+          const stateData = statesData.find(
+            (state) => state.equipmentId.toLowerCase() === machineIdLower
+          );
+          const latestStateId =
+            stateData?.states[
+              stateData.states.length - 1
+            ]?.equipmentStateId.toLowerCase();
+          const stateInfo = stateInfoMap[latestStateId] || {
+            name: "Desconhecido",
+            color: "#000",
+          };
+
+          return {
+            id: machine.equipmentId,
+            lat: machine.positions[machine.positions.length - 1].lat,
+            lng: machine.positions[machine.positions.length - 1].lon,
+            stateName: stateInfo.name,
+            stateColor: stateInfo.color,
+            modelName: modelName,
+          };
+        });
+
+        setMachines(processedMachines);
+      }
+    );
   }, []);
-
 
   return (
     <div style={{ height: "80vh", width: "50%" }}>
@@ -62,12 +123,19 @@ export const LocateMachines = () => {
         zoom={defaultZoom}
         style={{ height: "100%", width: "100%" }}
       >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {machines.map((machine) => (
           <Marker key={machine.id} position={[machine.lat, machine.lng]}>
             <Popup>
-              {machine.name}
+              <br />
+              Nome da maquina: {machine.modelName}
               <br />
               Máquina ID: {machine.id}
+              <br />
+              Estado:{" "}
+              <span style={{ color: machine.stateColor }}>
+                {machine.stateName}
+              </span>
             </Popup>
           </Marker>
         ))}
