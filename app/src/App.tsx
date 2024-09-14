0b78d682b2bc718da0517bc51a78e24391badaf7
vhforbes <vhforbes@gmail.com>
2024-09-14 15:57:58 -0300

    feat: initial maps integration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import equipamentsJson from "../data/equipment.json";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 function App() {
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   const [equipaments, setEquipaments] = useState(equipamentsJson);
+
+  console.log(API_KEY);
 
   return (
     <>
+      <APIProvider apiKey={API_KEY}>
+        <Map
+          style={{ width: "100vw", height: "100vh" }}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          defaultZoom={3}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        />
+      </APIProvider>
       {equipaments.map((equipament) => (
         <div key={equipament.id}>{equipament.name}</div>
       ))}
