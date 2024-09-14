6ac5b563f276c0757c63c8e6e2ece7570e696065
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-09-14 16:57:18 -0300

    feat: add markers consuming data

--- teste/app.vue
+++ teste/app.vue
@@ -12,6 +12,13 @@
         layer-type="base"
         name="OpenStreetMap"
       />
+      <LMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :lat-lng="[marker.lat, marker.lon]"
+        draggable
+      >
+      </LMarker>
     </LMap>
   </div>
 </template>
@@ -20,4 +27,36 @@
 import { ref } from 'vue'
 
 const zoom = ref(6)
+
+const store = useMyEquipmentStore()
+const { equipment, equipmentModel, equipmentPositionHistory, equipmentState, equipmentStateHistory } = storeToRefs(store)
+const { getAllData } = store
+
+await useAsyncData(getAllData)
+
+function getLatestItem(item) {
+  return item.reduce((latest, current) => {
+    return new Date(current.date) > new Date(latest.date) ? current : latest
+  })
+}
+
+const markers = computed(() => {
+  return equipment.value.map((equipment) => {
+    const positionHistory = equipmentPositionHistory.value.find(position => position.equipmentId == equipment.id)
+    const latestPosition = getLatestItem(positionHistory.positions)
+    const model = equipmentModel.value.find(model => model.id == equipment.equipmentModelId)
+    const stateHistory = equipmentStateHistory.value.find(state => state.equipmentId == equipment.id)
+    const latestState = getLatestItem(stateHistory.states)
+    const state = equipmentState.value.find(state => state.id == latestState.equipmentStateId)
+    return {
+      id: equipment.id,
+      name: equipment.name,
+      model: model.name,
+      lat: latestPosition.lat,
+      lon: latestPosition.lon,
+      state: state.name,
+      stateColor: state.color,
+    }
+  })
+})
 </script>
