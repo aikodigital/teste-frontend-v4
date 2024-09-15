ca56bfc9bc25ca52a99e6e75c51194321da68b1a
OtavioFSantos <otaviofs01@gmail.com>
2024-09-15 14:50:35 -0300

    feat: display markers and state

--- src/app/pages/map-view/map-view.component.ts
+++ src/app/pages/map-view/map-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import data from '../../../assets/data/equipmentPositionHistory.json'
+import positions from '../../../assets/data/equipmentPositionHistory.json'
+import states from '../../../assets/data/equipmentStateHistory.json'
+import stateInfo from '../../../assets/data/equipmentState.json'
 
 interface Position {
   date: string;
@@ -13,6 +15,20 @@ interface Equipment {
   positions: Position[];
 }
 
+interface EquipmentState {
+  equipmentId: string;
+  states: {
+    date: string;
+    equipmentStateId: string;
+  }[];
+}
+
+interface StateInfo {
+  id: string;
+  name: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-map-view',
   standalone: true,
@@ -22,7 +38,9 @@ interface Equipment {
 })
 export class MapViewComponent implements OnInit {
 
-  private equipamentPositions: any = data;
+  private equipamentPositions: any = positions;
+  private equipmentStates: any = states;
+  private stateInfo: any = stateInfo;
 
   ngOnInit(): void {
     this.initMap();
@@ -51,10 +69,20 @@ export class MapViewComponent implements OnInit {
   private displayEquipmentMarkers(map: L.Map): void {
     this.equipamentPositions.forEach((equipment: Equipment) => {
       const latestPosition = this.getLatestPosition(equipment.positions);
+      const latestState = this.getLatestStateInfo(equipment.equipmentId);
 
-      if (latestPosition) {
-        const marker = L.marker([latestPosition.lat, latestPosition.lon]).addTo(map);
-        marker.bindPopup(`<b>Equipment ID:</b> ${equipment.equipmentId}<br><b>Date:</b> ${latestPosition.date}`);
+      if (latestPosition && latestState) {
+        const customIcon = L.divIcon({
+          className: 'marker-icon',
+          html: `<div style="background-color: ${latestState.color}; width: 24px; height: 24px; border-radius: 50%; border: 2px solid white;"></div>`,
+        });
+
+        const marker = L.marker([latestPosition.lat, latestPosition.lon], { icon: customIcon }).addTo(map);
+        marker.bindPopup(
+          `<b>Equipamento:</b> ${equipment.equipmentId}<br>
+          <b>Data:</b> ${latestPosition.date}<br>
+          <b>Estado:</b> ${latestState.name}`
+        );
       }
     });
   }
@@ -65,5 +93,16 @@ export class MapViewComponent implements OnInit {
     }
 
     return positions.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
+  }
+
+  private getLatestStateInfo(equipmentId: string): StateInfo | undefined {
+    const equipmentState = this.equipmentStates.find((state: EquipmentState) => state.equipmentId === equipmentId);
+
+    if (!equipmentState || equipmentState.states.length === 0) {
+      return undefined;
+    }
+
+    const latestState = equipmentState.states.sort((a: { date: string | number | Date; }, b: { date: string | number | Date; }) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
+    return this.stateInfo.find((info: StateInfo) => info.id === latestState.equipmentStateId);
   }
 }
