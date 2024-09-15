bce3cd041d4506c32c4c28916eb50aa8cda9cf02
Alessandro Monteiro <alessandro.o.v.monteiro@gmail.com>
2024-09-15 13:19:29 -0300

    instalei as dependências para o leaflet-react; implementei um MapContainer e corrigi o bug no carregamento

--- teste-aiko/src/App.tsx
+++ teste-aiko/src/App.tsx
@@ -1,11 +1,21 @@
-//import "./App.css"
+import "./index.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
 const App = () => {
   return (
-    <div className="App">
-      <strong>Oi</strong>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   )
 }
 
