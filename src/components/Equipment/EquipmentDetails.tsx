8e125f6526d8cb3e39e34635d1b54d9ad2d88f20
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-09-16 16:00:54 -0300

    refactor: Update EquipmentDetails component to use useMarkers

--- src/components/Equipment/EquipmentDetails.tsx
+++ src/components/Equipment/EquipmentDetails.tsx
@@ -1,39 +1,57 @@
 import React from 'react';
 
-import { useEquipmentState, useEquipmentStateHistory } from '@/hooks';
+import {
+  EquipmentFromAPI,
+  EquipmentModelFromAPI,
+  EquipmentStateFromAPI,
+  EquipmentStateHistoryFromAPI
+} from '@/services/types';
 
 interface EquipmentDetailsProps {
   equipmentId: string;
+  stateHistory: Record<string, EquipmentStateHistoryFromAPI>;
+  states: EquipmentStateFromAPI[];
+  equipments: Record<string, EquipmentFromAPI>;
+  equipmentModels: Record<string, EquipmentModelFromAPI>;
 }
 
-export const EquipmentDetails: React.FC<EquipmentDetailsProps> = ({ equipmentId }) => {
-  const { data: stateHistory } = useEquipmentStateHistory();
-  const { data: states } = useEquipmentState();
+export const EquipmentDetails: React.FC<EquipmentDetailsProps> = ({
+  equipmentId,
+  stateHistory,
+  states,
+  equipments,
+  equipmentModels
+}) => {
+  const stateNames = states.reduce<Record<string, string>>((acc, state) => {
+    acc[state.id] = state.name;
+    return acc;
+  }, {});
 
-  const stateNames = states?.reduce(
-    (acc, state) => {
-      acc[state.id] = state.name;
-      return acc;
-    },
-    {} as Record<string, string>
-  );
-
-  const equipmentStateHistory = stateHistory?.[equipmentId];
+  const equipmentStateHistory = stateHistory[equipmentId];
   const equipmentCurrentStateId =
     equipmentStateHistory?.states?.[equipmentStateHistory.states.length - 1]?.equipmentStateId;
   const equipmentCurrentState = equipmentCurrentStateId
     ? (stateNames?.[equipmentCurrentStateId] ?? 'Desconhecido')
     : 'Desconhecido';
 
+  const equipment = equipments[equipmentId];
+  const equipmentModel = equipmentModels[equipment?.equipmentModelId ?? ''];
+
+  if (!equipment) {
+    return <div>Equipamento não encontrado.</div>;
+  }
+
   return (
     <div>
-      <h2>Equipamento: {equipmentId}</h2>
+      <h2>Nome: {equipment?.name}</h2>
+      <h2>Modelo: {equipmentModel?.name}</h2>
       <h2>Estado Atual: {equipmentCurrentState}</h2>
       <h3>Histórico de Estados:</h3>
       <ul>
         {equipmentStateHistory?.states.map((state) => (
           <li key={state.date}>
-            Data: {new Date(state.date).toLocaleString()} - Estado:{' '}
+            <strong>Data: </strong>
+            {new Date(state.date).toLocaleString()} - <strong>Estado: </strong>{' '}
             {stateNames?.[state.equipmentStateId] ?? 'Desconhecido'}
           </li>
         )) ?? 'Nenhum histórico disponível'}
