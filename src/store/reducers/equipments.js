593c3aa245e1bf06aa25c3c7ad3a01f1908f1774
Jpbepu <jpbepu@gmail.com>
2024-09-16 15:10:52 -0300

    funcao de formatacao de data corrigida para separar o dia do mes, e nao o dia da semana

--- src/store/reducers/equipments.js
+++ src/store/reducers/equipments.js
@@ -13,7 +13,7 @@ import equipmentStateHistory from '../../data/equipmentStateHistory.json'
 const formatDate = (dateString) => {
     const date = new Date(dateString)
 
-    const day = date.getDay().toString().padStart(2, '0')
+    const day = date.getDate().toString().padStart(2, '0')
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); //meses comecam no 0
     const year = date.getFullYear().toString().slice(-2)
 
