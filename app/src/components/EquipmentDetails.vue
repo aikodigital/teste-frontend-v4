0a32a1431243292d980da6c910a2d22390fa4282
Saviovd <savio.pinheiroal@gmail.com>
2024-09-16 14:23:07 -0400

    Add: main info about equipment

---
+++ app/src/components/EquipmentDetails.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="selectedEquipment" class="w-3/4 h-auto text-white">
+        <h2 class="text-white">Detalhes do Equipamento</h2>
+        <p><strong>Nome:</strong> {{ selectedEquipment.name }}</p>
+        <p><strong>Modelo:</strong> {{ selectedEquipment.model?.name }}</p>
+        <p><strong>Localização atual:</strong> {{ selectedEquipment.position.lat }}, {{ selectedEquipment.position.lng
+            }}</p>
+        <p><strong>status:</strong> {{ selectedEquipment.states.length > 0 ?
+            selectedEquipment.states[selectedEquipment.states.length -
+                1]?.name : 'Nenhum Status disponível' }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Equipment } from '../stores/equipmentStore';
+
+export default defineComponent({
+    props: {
+        selectedEquipment: {
+            type: Object as PropType<Equipment>,
+            required: false
+        }
+    }
+});
+</script>
