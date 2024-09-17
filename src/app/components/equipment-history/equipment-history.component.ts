6e3ceb52a324a36cb1162fca725da216446a3a5f
OtavioFSantos <otaviofs01@gmail.com>
2024-09-16 21:03:11 -0300

    feat: add state history

--- src/app/components/equipment-history/equipment-history.component.ts
+++ src/app/components/equipment-history/equipment-history.component.ts
@@ -1,24 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { MapService } from '../../services/map/map.service';
+import { EquipmentService } from '../../services/equipment/equipment.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-equipment-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './equipment-history.component.html',
   styleUrl: './equipment-history.component.css'
 })
-export class EquipmentHistoryComponent {
-  isSidebarOpen: boolean = true;
+export class EquipmentHistoryComponent implements OnChanges {
   @Input() equipment: any;
 
-  constructor(private mapService: MapService) { }
+  statesHistory: any;
+  isSidebarOpen: boolean = true;
+
+  constructor(
+    private mapService: MapService,
+    private equipmentService: EquipmentService
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['equipment'] && changes['equipment'].currentValue) {
+      this.loadEquipmentHistory();
+    }
+  }
+
+  loadEquipmentHistory() {
+    this.statesHistory = this.equipmentService.getStateHistoryByEquipmentId(this.equipment.equipmentId);
+  }
 
   closeSidebar() {
     this.mapService.closeSidebar();
   }
 
-  get isOpenClass(): string {
-    return this.isSidebarOpen ? 'open' : '';
+  formatDate(dateString: string): string {
+    if (!dateString) {
+      return '';
+    }
+    const data = new Date(dateString);
+
+    if (isNaN(data.getTime())) {
+      return '';
+    }
+
+    const dia = String(data.getUTCDate()).padStart(2, '0');
+    const mes = String(data.getUTCMonth() + 1).padStart(2, '0');
+    const ano = data.getUTCFullYear();
+    const horas = String(data.getUTCHours()).padStart(2, '0');
+    const minutos = String(data.getUTCMinutes()).padStart(2, '0');
+
+    return `${dia}/${mes}/${ano} - ${horas}:${minutos}`;
   }
 }
