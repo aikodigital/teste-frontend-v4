d1cbd3e8aaffe39c51092709eee7a05cb1929967
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-09-17 09:39:14 -0300

    feat: add marker highlight and 'flyTo' when opening drawer

--- teste/app.vue
+++ teste/app.vue
@@ -22,16 +22,18 @@
       <LMarker
         v-for="marker in markers"
         :key="marker.id"
-        :lat-lng="[marker.lat, marker.lon]"
-        @click="openDrawer(marker.id)"
+        :lat-lng="marker.position"
+        @click="openDrawer(marker.id, marker.position)"
         >
           <LIcon
           :tooltip-anchor="[25, -25]"
-            :icon-anchor="[9, 39]"
+          :icon-anchor="selectedMarkerId == marker.id ? [16, 60] : [11, 41]"
           class="relative"
           >
-          <div :class="`text-2xl ${marker.icon}`" />
-            <div class="i-ph-caret-down-fill" />
+          <div :style="`${selectedMarkerId == marker.id ? 'filter: drop-shadow(0px 0px 1px dodgerblue);' : ''}`">
+            <div :class="`${marker.icon} ${selectedMarkerId == marker.id ? 'text-4xl' : 'text-2xl'}`" />
+            <div :class="`i-ph-caret-down-fill ${selectedMarkerId == marker.id ? 'text-4xl text-blue-500' : 'text-2xl'}`" />
+          </div>
           <Badge
             v-if="marker.state !== 'Operando'"
             pt:root:class="absolute top-0 right-0 w-[10px] h-[10px] rounded"
@@ -55,13 +57,22 @@
           </LTooltip>
         <Drawer
           v-if="selectedMarkerId == marker.id"
-          v-model:visible="isDrawerVisible"
+          :visible="true"
           position="right"
           :modal="false"
           :dismissable="false"
           class="bg-white"
           pt:mask:style="pointer-events: none !important;"
         >
+          <template #closeicon>
+            <Button
+              size="small"
+              text
+              @click="selectedMarkerId = null"
+            >
+              X
+            </Button>
+          </template>
           <template #header>
             <div>
               <h3 class="mt-0 mb-0">
@@ -85,9 +96,9 @@
 </template>
 
 <script setup>
-const zoom = ref(11)
+const map = ref(null)
 
-const isDrawerVisible = ref(false)
+const zoom = ref(11)
 
 const store = useMyEquipmentStore()
 const { equipment, equipmentModel, equipmentPositionHistory, equipmentState, equipmentStateHistory } = storeToRefs(store)
@@ -95,9 +106,18 @@ const { getAllData } = store
 
 const selectedMarkerId = ref(null)
 
-function openDrawer(markerId) {
+function flyToMarker(position) {
+  const mapObject = map.value.leafletObject
+  const zoom = 15
+  const drawerWidth = 320
+  const mapWidth = mapObject.getSize().x
+  const lonOffset = (drawerWidth / mapWidth) * 360 / Math.pow(2, zoom)
+  mapObject.flyTo([position.lat, position.lon + lonOffset], zoom)
+}
+
+function openDrawer(markerId, position) {
   selectedMarkerId.value = markerId
-  isDrawerVisible.value = true
+  flyToMarker(position)
 }
 
 await useAsyncData(() => getAllData().then(() => true))
@@ -132,8 +152,7 @@ const markers = computed(() => {
       id: equipment.id,
       name: equipment.name,
       model: model.name,
-      lat: latestPosition.lat,
-      lon: latestPosition.lon,
+      position: latestPosition,
       state: state.name,
       stateColor: state.color,
       stateHistory: stateHistory,
