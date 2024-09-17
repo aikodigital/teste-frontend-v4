5e0e7fc1f33d1656d6ca82996a277ffbb29b69b6
Saviovd <savio.pinheiroal@gmail.com>
2024-09-17 13:12:59 -0400

    Update: main page with all equipment data

--- app/src/views/App.vue
+++ app/src/views/App.vue
@@ -1,73 +1,147 @@
 <template>
-  <div class="p-8 bg-gray-950">
-    <div class="flex">
-      <div class="w-1/4 pr-4">
-        <h2 class="text-white">Lista de Equipamentos</h2>
-        <ul class="list-disc pl-5 text-white">
-          <li v-for="equipment in equipments" :key="equipment.id" class="cursor-pointer hover:underline"
-            @click="updateMapLocation(equipment.id)">
-            {{ equipment.name }}
-          </li>
-        </ul>
-      </div>
-      <div class="w-3/4">
-        <EquipmentMap ref="equipmentMap" :initialPosition="{ lat: -19.126536, lng: -45.947756 }" />
-      </div>
-    </div>
-    <div v-if="selectedEquipment" class="mt-4">
-      <h2 class="text-white">Detalhes do Equipamento</h2>
-      <p><strong>Nome:</strong> {{ selectedEquipment.name }}</p>
-      <p><strong>Latitude:</strong> {{ selectedEquipment.position.lat }}</p>
-      <p><strong>Longitude:</strong> {{ selectedEquipment.position.lng }}</p>
+  <div class="p-8 bg-gray-950 w-full h-auto flex">
+    <EquipmentList :equipments="equipments" :selectedEquipment="selectedEquipment"
+      :handleEquipmentClick="handleEquipmentClick" :states="states" :models="models" />
+    <div v-if="selectedEquipment" class="w-full">
+      <EquipmentDetails :selectedEquipment="selectedEquipment" />
+      <EquipmentMap v-if="selectedEquipment" :key="selectedEquipment.id" ref="equipmentMap"
+        :initialPosition="mapPosition" :selectedEquipment="selectedEquipment"
+        :positionHistory="selectedEquipment.positionHistory" :view-path="viewPath" />
+      <button @click="toggleViewPath" class="btn mt-4 text-white bg-orange-500">
+        {{ viewPath ? 'Esconder' : 'Ver' }} o trajeto do equipamento
+      </button>
+      <EquipmentStateHistory :stateHistory="selectedEquipment.stateHistory" :chartData="chartData" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-import EquipmentMap from '../components/EquipmentMap.vue';
+import { ref, onMounted } from 'vue';
+import EquipmentList from '../components/EquipmentList.vue';
+import EquipmentDetails from '../components/EquipmentDetails.vue';
+import EquipmentStateHistory from '../components/EquipmentStateHistory.vue';
 import { getEquipmentPosition } from '../utils/getPosition.ts';
-import { useEquipmentStore } from '../stores/equipmentStore.ts';
+import { Equipment, useEquipmentStore } from '../stores/equipmentStore.ts';
+import EquipmentMap from '../components/EquipmentMap.vue';
+
+interface EquipmentMapInstance {
+  drawPath: (positions: { lat: number; lon: number }[]) => void;
+  updatePosition: (position: { lat: number; lon: number }) => void;
+  updateMarker: (position: { lat: number; lon: number }) => void;
+}
+
+interface ChartData {
+  labels: string[];
+  datasets: { label: string; data: number[] }[];
+}
 
 export default {
   components: {
-    EquipmentMap
+    EquipmentList,
+    EquipmentDetails,
+    EquipmentStateHistory,
+    EquipmentMap,
   },
   setup() {
     const store = useEquipmentStore();
-    const selectedEquipment = ref<{ id: string, name: string, position: { lat: number, lng: number } } | null>(null);
-    const equipmentMap = ref<any>(null);
-
-    const equipments = store.equipments.map(equipment => ({
+    const equipments = ref(store.equipments.map((equipment) => ({
       ...equipment,
-      position: getEquipmentPosition(equipment.id)
-    }));
-    const getPosition = (id: string) => {
-      const position = getEquipmentPosition(id);
-      return position ? { lat: position.lat, lng: position.lon } : { lat: 0, lng: 0 };
+      position: getEquipmentPosition(equipment.id) || { lat: 0, lon: 0 },
+      states:
+        store.getStateHistory(equipment.id)?.states.map(({ equipmentStateId }) =>
+          store.states.find((state) => state.id === equipmentStateId) || { id: '', name: 'Desconhecido', color: '#000000' }
+        ) || [],
+      model: store.getModel(equipment.equipmentModelId),
+    })));
+
+    const states = store.states;
+    const models = store.models;
+    const selectedEquipment = ref<Equipment | null>(null);
+    const viewPath = ref(false);
+
+    const chartData = ref<ChartData>({
+      labels: [],
+      datasets: [{ label: 'Histórico de Estados', data: [0] }],
+    });
+
+    const mapPosition = ref({ lat: 0, lon: 0 });
+    const equipmentMapRef = ref<EquipmentMapInstance | null>(null);
+
+    const handleEquipmentClick = (equipment: Equipment) => {
+      const stateHistory =
+        store.getStateHistory(equipment.id)?.states.map(({ date, equipmentStateId }) => {
+          const state = store.states.find((state) => state.id === equipmentStateId);
+          return { date, name: state ? state.name : 'Desconhecido' };
+        }) || [];
+
+      const positionHistory =
+        store.getPositionHistory(equipment.id)?.positions.map(({ date, lat, lon }) => ({
+          date,
+          lat,
+          lon,
+        })) || [];
+
+      viewPath.value = false;
+      selectedEquipment.value = { ...equipment, stateHistory, positionHistory, state: states[states.length - 1], model: store.getModel(equipment.equipmentModelId) };
+      updateChartData();
+
+      mapPosition.value = getEquipmentPosition(equipment.id) || { lat: 0, lon: 0 };
+      if (equipmentMapRef.value) {
+        equipmentMapRef.value.updatePosition(mapPosition.value);
+        equipmentMapRef.value.updateMarker(mapPosition.value);
+      }
     };
 
-    function handleEquipmentClick(equipment: { id: string, name: string }) {
-      selectedEquipment.value = { ...equipment, position: getPosition(equipment.id) };
-    }
-
-    function updateMapLocation(equipmentId: string) {
-      const position = getEquipmentPosition(equipmentId);
-      if (position) {
-        const formattedPosition = { lat: position.lat, lng: position.lon };
-        if (equipmentMap.value) {
-          equipmentMap.value.updatePosition(formattedPosition);
-        }
+    const toggleViewPath = () => {
+      viewPath.value = !viewPath.value;
+      if (viewPath.value && selectedEquipment.value) {
+        drawEquipmentPath();
       }
-    }
+    };
+
+    const updateChartData = () => {
+      if (selectedEquipment.value) {
+        const history = selectedEquipment.value.stateHistory;
+        const stateCounts: { [key: string]: number } = {};
+        history.forEach((item) => {
+          stateCounts[item.name] = (stateCounts[item.name] || 0) + 1;
+        });
+
+        chartData.value = {
+          labels: Object.keys(stateCounts),
+          datasets: [{ label: 'Histórico de Estados', data: Object.values(stateCounts) }],
+        };
+      }
+    };
+
+    const drawEquipmentPath = () => {
+      if (selectedEquipment.value && selectedEquipment.value.positionHistory && equipmentMapRef.value) {
+        const positions = selectedEquipment.value.positionHistory.map((pos) => ({
+          lat: pos.lat,
+          lon: pos.lon,
+        }));
+        equipmentMapRef.value.drawPath(positions);
+      }
+    };
+
+    onMounted(() => {
+      if (equipments.value.length > 0) {
+        handleEquipmentClick(equipments.value[0]);
+      }
+    });
 
     return {
       equipments,
       selectedEquipment,
       handleEquipmentClick,
-      updateMapLocation,
-      equipmentMap
+      chartData,
+      states,
+      models,
+      mapPosition,
+      equipmentMapRef,
+      toggleViewPath,
+      viewPath,
     };
-  }
+  },
 };
 </script>
