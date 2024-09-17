535d84a5d889b49a22678af0975f7ff2dd3a07b0
Alessandro Monteiro <alessandro.o.v.monteiro@gmail.com>
2024-09-17 12:55:27 -0300

    implementei a página da lista de caminhões; criei um footer e estilizei; adicionei um input de busca na página, mas ainda não é funcional

--- teste-aiko/src/components/Navbar/index.tsx
+++ teste-aiko/src/components/Navbar/index.tsx
@@ -1,18 +1,17 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
-    const location = useLocation();
     const navigate = useNavigate();
 
     return (
             <nav className={styles.nav}>
-                <img className={styles.logo} onClick={() => navigate('/')} />
+                <img src="src/assets/aiko.png" className={styles.logo} onClick={() => navigate('/')} />
                 <div className={styles.links}>
 
                     <div>
                         <Link to='/' className={styles.link}>
-                            Home
+                            Map
                         </Link>
                     </div>
                     <div>
