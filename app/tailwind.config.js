4e8cd9b6756227cd7e99b1035218eb8757271be7
Saviovd <savio.pinheiroal@gmail.com>
2024-09-17 22:13:01 -0400

    Add: equipent list responsiviness

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
    theme: {
-      extend: {},
+      extend: {
+         screens: {
+            'xs': '480px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+         },
+      },
    },
    plugins: [],
 };
