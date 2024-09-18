8b4781a3d9a9f6b79b4568b8c8166457cd969e0a
Saviovd <savio.pinheiroal@gmail.com>
2024-09-18 00:37:47 -0400

    Update: equipment details responsiviness

--- app/src/components/EquipmentDetails.vue
+++ app/src/components/EquipmentDetails.vue
@@ -1,22 +1,25 @@
 <template>
-    <div v-if="selectedEquipment" class="w-3/4 h-auto text-white space-y-3">
-        <h2 class="text-white font-semibold text-2xl uppercase">Detalhes do Equipamento</h2>
-        <p><span class="mr-2 font-medium">Nome:</span> {{ selectedEquipment.name }}</p>
-        <p><span class="mr-2 font-medium">Modelo:</span> {{ selectedEquipment.model?.name }}</p>
-        <p><span class="mr-2 font-medium">Ultima localização:</span> {{ selectedEquipment.position.lat }}, {{
-            selectedEquipment.position.lon
-            }}</p>
-        <div class="flex mt-2 items-center">
-            <span class="mr-2 font-medium">status:</span>
-            <div class="bg-gray-700 flex gap-1 justify-center items-center py-2 px-4 rounded-3xl">
-                <div class="size-3 rounded-full" :style="{
-                    backgroundColor: selectedEquipment.states[selectedEquipment.states.length -
-                        1]?.color
-                }"></div>
-                <span class="">
-                    {{ selectedEquipment.states.length > 0 ?
-                    selectedEquipment.states[selectedEquipment.states.length -
-                    1]?.name : 'Nenhum Status disponível' }}
+    <div v-if="selectedEquipment" class="w-full max-w-4xl bg-gray-900 rounded-lg shadow-lg text-white space-y-4">
+        <h2 class="text-white font-semibold text-lg md:text-2xl uppercase mb-3">Detalhes do Equipamento</h2>
+
+        <div class="space-y-2">
+            <p class="text-md"><span class="font-semibold">Nome:</span> {{ selectedEquipment.name }}</p>
+            <p class="text-md"><span class="font-semibold">Modelo:</span> {{ selectedEquipment.model?.name }}</p>
+            <p class="text-md"><span class="font-semibold">Última Localização:</span> {{ selectedEquipment.position.lat
+                }}, {{ selectedEquipment.position.lon }}</p>
+        </div>
+
+        <div class="flex items-center gap-2 mt-4">
+            <span class="font-semibold text-md">Status:</span>
+            <div class="flex items-center py-2 px-4 bg-gray-800 rounded-full">
+                <div class="w-4 h-4 rounded-full"
+                    :style="{ backgroundColor: selectedEquipment.states[selectedEquipment.states.length - 1]?.color || '#gray' }">
+                </div>
+                <span class="ml-2">
+                    {{ selectedEquipment.states.length > 0
+                        ? selectedEquipment.states[selectedEquipment.states.length - 1]?.name
+                        : 'Nenhum Status disponível'
+                    }}
                 </span>
             </div>
         </div>
