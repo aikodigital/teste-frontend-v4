163f96e7e756c79638469ea0784c5968149ac70d
Saviovd <savio.pinheiroal@gmail.com>
2024-09-17 21:46:55 -0400

    Add: details styles

--- app/src/components/EquipmentDetails.vue
+++ app/src/components/EquipmentDetails.vue
@@ -1,13 +1,25 @@
 <template>
-    <div v-if="selectedEquipment" class="w-3/4 h-auto text-white">
-        <h2 class="text-white">Detalhes do Equipamento</h2>
-        <p><strong>Nome:</strong> {{ selectedEquipment.name }}</p>
-        <p><strong>Modelo:</strong> {{ selectedEquipment.model?.name }}</p>
-        <p><strong>Localização atual:</strong> {{ selectedEquipment.position.lat }}, {{ selectedEquipment.position.lon
+    <div v-if="selectedEquipment" class="w-3/4 h-auto text-white space-y-3">
+        <h2 class="text-white font-semibold text-2xl uppercase">Detalhes do Equipamento</h2>
+        <p><span class="mr-2 font-medium">Nome:</span> {{ selectedEquipment.name }}</p>
+        <p><span class="mr-2 font-medium">Modelo:</span> {{ selectedEquipment.model?.name }}</p>
+        <p><span class="mr-2 font-medium">Ultima localização:</span> {{ selectedEquipment.position.lat }}, {{
+            selectedEquipment.position.lon
             }}</p>
-        <p><strong>status:</strong> {{ selectedEquipment.states.length > 0 ?
-            selectedEquipment.states[selectedEquipment.states.length -
-                1]?.name : 'Nenhum Status disponível' }}</p>
+        <div class="flex mt-2 items-center">
+            <span class="mr-2 font-medium">status:</span>
+            <div class="bg-gray-700 flex gap-1 justify-center items-center py-2 px-4 rounded-3xl">
+                <div class="size-3 rounded-full" :style="{
+                    backgroundColor: selectedEquipment.states[selectedEquipment.states.length -
+                        1]?.color
+                }"></div>
+                <span class="">
+                    {{ selectedEquipment.states.length > 0 ?
+                    selectedEquipment.states[selectedEquipment.states.length -
+                    1]?.name : 'Nenhum Status disponível' }}
+                </span>
+            </div>
+        </div>
     </div>
 </template>
 
