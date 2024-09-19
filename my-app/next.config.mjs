b449fa3ac63015eb4df69819eea8cb198221ce27
SilvestreL <lucassilvestreadv@gmail.com>
2024-09-19 10:29:33 -0300

    ignore eslint

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
