b821aed6dfc7c5b5c5dc0c685d3cce2065760e32
Maisa Preis <maisapierinipreis@gmail.com>
2024-09-20 13:37:13 -0300

    feat: Adicionando um header no sistema.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <main class="content-area">
+        <HeaderApp />
         <MapView />
         <StateHistory v-if="stateHistoryStore.showStateHistory" />
     </main>
 </template>
 
 <script setup lang="ts">
+import HeaderApp from "./components/HeaderApp.vue";
 import MapView from "./components/MapView.vue";
 import StateHistory from "./components/StateHistory.vue";
 import { useStateHistoryStore } from "@/stores/stateHistory.ts";
@@ -16,5 +18,6 @@ const stateHistoryStore = useStateHistoryStore();
 <style scoped>
 .content-area {
     display: flex;
+    margin-top: 40px;
 }
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header-area">
+        <img src="@/assets/aiko.png" alt="Company logo">
+        <h1>Localizador de Equipamentos</h1>
+    </div>
+</template>
+
+<style scoped>
+.header-area {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    padding: 0 30px;
+
+    background-color: var(--indigo-dark-blue);
+    box-shadow: 0 8px 6px -6px rgb(0, 0, 0);
+    width: 100vw;
+    height: 70px;
+}
+
+img {
+    height: 70%;
+    margin-right: 30px;
+}
+
+h1 {
+    color: var(--light-gray-2);
+}
+</style>
