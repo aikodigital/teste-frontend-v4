9333e9495bca472eeba608f8be52aebdcf365504
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-10-28 01:48:58 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { ModelSelect } from '@/components/model-selected'
-
+import ContainerMap from '@/components/container-map'
 import { StatusSelect } from '@/components/status-select'
 import CarouselEquipments from '@/components/carousel-equipment'
-import ContainerMap from '@/components/container-map'
 
 export default function Home() {
   return (
