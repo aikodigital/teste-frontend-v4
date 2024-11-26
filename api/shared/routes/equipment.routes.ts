3635caa0eff78572ff8ba47b4257b7bd44b72ed8
psbrunosouza <psbrunosouza@gmail.com>
2024-11-26 06:53:38 -0300

    refact: clear ssr unused structures

--- api/shared/routes/equipment.routes.ts
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-
-// const equipmentRoutes = express.Router();
-
-// equipmentRoutes.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// export default equipmentRoutes;
--- server.ts
+++ server.ts
@@ -45,10 +45,7 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
-  // Start up the Node server
   const server = app();
-
-  // server.use('/api/equipments', equipmentRoutes);
 
   server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
