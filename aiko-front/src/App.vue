10a44ad395347a5fbc24e4cd5f0bc02314d4588d
Erico Alexandre <ericoabs25@gmail.com>
2025-01-22 15:23:03 -0300

    first commit

---
+++ aiko-front/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HomeView from './views/HomeView.vue';
+
+</script>
+
+<template>
+  <HomeView />
+</template>
---
+++ aiko-front/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>Hello</p>
+  </main>
+</template>
