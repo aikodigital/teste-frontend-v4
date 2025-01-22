b1ab600c16646c73381b762ca83dfb931169f392
Erico Alexandre <ericoabs25@gmail.com>
2025-01-22 19:45:58 -0300

    feat: add markers on map for equipments last position and it's state

--- aiko-front/src/components/Map.vue
+++ aiko-front/src/components/Map.vue
@@ -2,6 +2,10 @@
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import { onMounted } from 'vue'
+import equipmentPositionHistory from '../assets/data/equipmentPositionHistory.json'
+import equipmentStateHistory from '../assets/data/equipmentStateHistory.json'
+import type { EquipmentPositionHistory, EquipmentStateHistory } from '@/types/equipment'
+import { states } from '@/constants/states'
 
 let map = undefined
 
@@ -10,6 +14,27 @@ const createMapLayer = () => {
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
+
+  if (equipmentPositionHistory.length) {
+    setMarkers()
+  }
+}
+
+const getStateById = (id: string) => {
+  return states.find((state) => state.id === id) || null;
+}
+
+const setMarkers = () => {
+  equipmentPositionHistory.map((equipment: EquipmentPositionHistory) => {
+    const currentPosition = equipment.positions.slice(-1)
+    if (!currentPosition) return
+    const equipmentState = equipmentStateHistory.find((eq: EquipmentStateHistory) => eq.equipmentId === equipment.equipmentId)
+    if (!equipmentState) return
+    const lastState = equipmentState.states.slice(-1)
+    const state = (getStateById(lastState[0]?.equipmentStateId))
+
+    return L.marker([currentPosition[0].lat, currentPosition[0].lon]).addTo(map).bindPopup('opa').bindTooltip(state?.name || '').openTooltip();
+  })
 }
 
 onMounted(() => {
