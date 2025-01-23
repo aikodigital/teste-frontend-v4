dc7c1aa8b1c14f10e497d7b3d997ffd11421785a
Erico Alexandre <ericoabs25@gmail.com>
2025-01-23 18:36:40 -0300

    feat: adding popup information, tooltip information and splitting code into separate components

--- aiko-front/src/components/Map.vue
+++ aiko-front/src/components/Map.vue
@@ -4,9 +4,10 @@ import L from 'leaflet'
 import { onMounted, ref } from 'vue'
 import equipmentPositionHistory from '../assets/data/equipmentPositionHistory.json'
 import equipmentStateHistory from '../assets/data/equipmentStateHistory.json'
-import type { Equipment, EquipmentModel, EquipmentPositionHistory, EquipmentState, EquipmentStateHistory } from '@/types/equipment'
+import type { DetailedStateHistory, Equipment, EquipmentModel, EquipmentPositionHistory, EquipmentStateHistory } from '@/types/equipment'
 import { useEquipment } from '@/hooks/useEquipment'
-import { format } from 'date-fns'
+import MapLegend from './MapLegend.vue'
+import EquipmentHistory from './EquipmentHistory.vue'
 
 let map: L.Map
 
@@ -18,13 +19,13 @@ const {
   getEquipmentModel
 } = useEquipment()
 
-const equipmentStateList = ref<(EquipmentState | null)[]>([])
+const equipmentStateList = ref<DetailedStateHistory[]>([])
 
 const equipmentType = ref<Equipment>()
 const equipmentModel = ref<EquipmentModel>()
 
 const createMapLayer = () => {
-  map = L.map('mapContainer').setView([-19.126536, -45.947756], 9)
+  map = L.map('mapContainer').setView([-19.126536, -45.947756], 10.5)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
@@ -36,12 +37,17 @@ const createMapLayer = () => {
 
 const getEquipmentInformation = (equipmentStateHistory: EquipmentStateHistory) => {
   equipmentStateList.value = getStateHistory(equipmentStateHistory)
+  console.log(equipmentStateList.value)
   equipmentType.value = getEquipmentType(equipmentStateHistory.equipmentId)
-  equipmentModel.value = getEquipmentModel(equipmentType.value.equipmentModelId)
+  equipmentModel.value = getEquipmentModel(equipmentType?.value.equipmentModelId)
 }
 
-const toDate = (date) => format(new Date(date), "dd/MM/yyyy")
-const toHour = (date) => format(new Date(date), "HH:mm")
+const popUp = (eqName: string, state: string, eqType) => (
+  `<span>Nome do Equipamento: ${eqName}</span>
+  <br />
+  <span>Tipo do Equipamento: ${eqType}</span>
+  <br />
+  <span>Estado Atual: ${state}</span>`)
 
 const setMarkers = () => {
   equipmentPositionHistory.map((equipment: EquipmentPositionHistory) => {
@@ -51,8 +57,14 @@ const setMarkers = () => {
     if (!equipmentState) return
     const lastState = equipmentState.states.slice(-1)
     const state = (getStateById(lastState[0]?.equipmentStateId))
+    const equipmentName = getEquipmentType(equipmentState.equipmentId)
+    const typeName = getEquipmentModel(equipmentName?.equipmentModelId || '')
 
-    return L.marker([currentPosition[0].lat, currentPosition[0].lon], { icon: getMarkerColor(state?.color || '') }).addTo(map).on('click', () => getEquipmentInformation(equipmentState)).bindPopup('opa').bindTooltip(state?.name || '').openTooltip();
+    return L.marker([currentPosition[0].lat, currentPosition[0].lon], { icon: getMarkerColor(state?.color || '') })
+      .addTo(map).on('click', () => getEquipmentInformation(equipmentState))
+      .bindPopup(popUp(equipmentName?.name || '', state?.name || '', typeName?.name || ''))
+      .bindTooltip(typeName?.name || '')
+      .openTooltip();
   })
 }
 
@@ -62,49 +74,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex">
-    <div id="mapContainer"></div>
-
-    <div class="w-2/4 p-4">
-      <div>
-        <h3>Equipment Information</h3>
-        <p>Equipment Type: {{ equipmentModel?.name }}</p>
-        <p>Equipment Name: {{ equipmentType?.name }}</p>
-
-        <span :class="`h-4 w-4 bg-[#2ecc71] rounded-full`"></span>
-        <span :class="`h-4 w-4 bg-[#f1c40f] rounded-full`"></span>
-        <span :class="`h-4 w-4 bg-[#e74c3c] rounded-full`"></span>
-      </div>
-
-      <div v-if="equipmentStateList.length" class="flex flex-col w-full">
-        <div class="flex items-center justify-between border-b border-gray-600 pb-2 mb-2 w-70">
-          <div class="flex items-center w-full">
-            <span class="text-gray-400 font-medium">Estado</span>
-          </div>
-          <div class="flex w-full justify-between">
-            <span class="text-gray-400 font-medium">Data</span>
-            <span class="text-gray-400 font-medium">Hora</span>
-          </div>
-        </div>
-        <div class="flex items-center w-70 justify-between" v-for="(item, index) in equipmentStateList">
-          <div class="flex items-center w-full py-1">
-            <span :class="`h-4 w-4 bg-[${item?.states.color}] rounded-full`"></span>
-            <span class="text-white pl-2" :key="index">{{ item?.states.name }} </span>
-          </div>
-          <div class="flex w-full justify-between">
-            <span class="text-white pr-4" :key="index">{{ toDate(item.date) }} </span>
-            <span class="text-white" :key="index">{{ toHour(item.date) }} </span>
-          </div>
-        </div>
-
-      </div>
+  <div class="flex flex-col lg:flex-row w-full justify-between">
+    <div class="flex flex-col pr-2">
+      <div id="mapContainer" class="h-125 w-125 sm:w-150 md:w-180 lg:w-125 xl:w-150 2xl:w-175" />
+      <MapLegend />
     </div>
+    <EquipmentHistory :equipment-model="equipmentModel" :equipment-state-list="equipmentStateList" :equipment-type="equipmentType" />
   </div>
 </template>
-
-<style scoped>
-#mapContainer {
-  height: 700px;
-  width: 700px;
-}
-</style>
--- aiko-front/src/views/HomeView.vue
+++ aiko-front/src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import Map from '@/components/Map.vue';
+import logo from '@/assets/img/aiko.png'
 
 
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col p-6 lg:max-w-full items-center">
+    <div class="h-12 w-25 mb-8 self-center">
+      <img :src="logo" alt="aiko logo" />
+    </div>
+    <div class="flex flex-col lg:max-w-full items-center">
+      <div class="w-full lg:p-6  rounded-lg shadow-lg text-center max-w-125 lg:max-w-full">
+        <h2 class="text-2xl font-bold text-cyan-400 mb-4">Explore os Equipamentos</h2>
+        <p class="text-gray-300 text-lg mb-6">
+          Clique em um marcador abaixo para exibir informações detalhadas de cada equipamento.
+        </p>
+      </div>
+    </div>
     <Map />
   </main>
 </template>
