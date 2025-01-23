f0ff68b93be8ca9b1c1eacd2886595b593d67ed6
Erico Alexandre <ericoabs25@gmail.com>
2025-01-23 18:38:24 -0300

    refactor: spllitting the header into a separate component

--- aiko-front/src/views/HomeView.vue
+++ aiko-front/src/views/HomeView.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 import Map from '@/components/Map.vue';
-import logo from '@/assets/img/aiko.png'
+import Header from '@/components/Header.vue';
+
 
 
 </script>
 
 <template>
   <main class="flex flex-col p-6 lg:max-w-full items-center">
-    <div class="h-12 w-25 mb-8 self-center">
-      <img :src="logo" alt="aiko logo" />
-    </div>
-    <div class="flex flex-col lg:max-w-full items-center">
-      <div class="w-full lg:p-6  rounded-lg shadow-lg text-center max-w-125 lg:max-w-full">
-        <h2 class="text-2xl font-bold text-cyan-400 mb-4">Explore os Equipamentos</h2>
-        <p class="text-gray-300 text-lg mb-6">
-          Clique em um marcador abaixo para exibir informações detalhadas de cada equipamento.
-        </p>
-      </div>
-    </div>
+    <Header />
     <Map />
   </main>
 </template>
