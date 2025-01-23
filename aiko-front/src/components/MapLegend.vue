ef5154b09866d26435349dbca62f08b59346b514
Erico Alexandre <ericoabs25@gmail.com>
2025-01-23 18:34:51 -0300

    feat: creating a component for the map legend

---
+++ aiko-front/src/components/MapLegend.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import greenMarker from '../assets/icons/marker-icon-2x-green.png'
+import yellowMarker from '../assets/icons/marker-icon-2x-yellow.png'
+import redMarker from '../assets/icons/marker-icon-2x-red.png'
+</script>
+
+<template>
+  <div class="flex py-4 items-center xl:justify-end text-white">
+    <h4>Legenda: </h4>
+    <div class="flex px-2 items-center">
+      <p class="pr-2">Operando</p>
+      <img :src="greenMarker" alt="marcador verde" class="h-9 w-6" />
+    </div>
+    <div class="flex px-1 items-center">
+      <p class="pr-2">Manutenção</p>
+      <img :src="redMarker" alt="marcador vermelho" class="h-9 w-6" />
+    </div>
+    <div class="flex px-1 items-center">
+      <p class="pr-2">Parado</p>
+      <img :src="yellowMarker" alt="marcador amarelo" class="h-9 w-6" />
+    </div>
+  </div>
+</template>
