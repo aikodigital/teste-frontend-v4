6ff0d75a446a9a0921ab2d2d10efd7857a6054ae
Erico Alexandre <ericoabs25@gmail.com>
2025-01-23 10:26:16 -0300

    Revert "feature: creating more functions for the useequipment hook to retrieve important information"
    This reverts commit cd0aa15fe95fcd15bdd4961843bba682de87d2c4.

--- aiko-front/src/components/Map.vue
+++ aiko-front/src/components/Map.vue
@@ -1,26 +1,13 @@
 <script lang="ts" setup>
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import equipmentPositionHistory from '../assets/data/equipmentPositionHistory.json'
 import equipmentStateHistory from '../assets/data/equipmentStateHistory.json'
-import type { Equipment, EquipmentModel, EquipmentPositionHistory, EquipmentState, EquipmentStateHistory } from '@/types/equipment'
+import type { EquipmentPositionHistory, EquipmentStateHistory } from '@/types/equipment'
 import { useEquipment } from '@/hooks/useEquipment'
 
 let map: L.Map
-
-const {
-  getMarkerColor,
-  getStateById,
-  getStateHistory,
-  getEquipmentType,
-  getEquipmentModel
-} = useEquipment()
-
-const equipmentStateList = ref<(EquipmentState | null)[]>([])
-
-const equipmentType = ref<Equipment>()
-const equipmentModel = ref<EquipmentModel>()
 
 const createMapLayer = () => {
   map = L.map('mapContainer').setView([-19.126536, -45.947756], 9)
@@ -33,14 +20,7 @@ const createMapLayer = () => {
   }
 }
 
-const getEquipmentInformation= (equipmentStateHistory: EquipmentStateHistory) => {
-  equipmentStateList.value = getStateHistory(equipmentStateHistory)
-  console.log(equipmentStateList.value)
-  equipmentType.value = getEquipmentType(equipmentStateHistory.equipmentId)
-  equipmentModel.value = getEquipmentModel(equipmentType.value.equipmentModelId)
-  // console.log(equipmentType.value)
-  // console.log(equipmentModel.value)
-}
+const { getMarkerColor, getStateById } = useEquipment()
 
 const setMarkers = () => {
   equipmentPositionHistory.map((equipment: EquipmentPositionHistory) => {
@@ -51,7 +31,7 @@ const setMarkers = () => {
     const lastState = equipmentState.states.slice(-1)
     const state = (getStateById(lastState[0]?.equipmentStateId))
 
-    return L.marker([currentPosition[0].lat, currentPosition[0].lon], { icon: getMarkerColor(state?.color || '') }).addTo(map).on('click', () => getEquipmentInformation(equipmentState)).bindPopup('opa').bindTooltip(state?.name || '').openTooltip();
+    return L.marker([currentPosition[0].lat, currentPosition[0].lon], { icon: getMarkerColor(state?.color || '') }).addTo(map).bindPopup('opa').bindTooltip(state?.name || '').openTooltip();
   })
 }
 
@@ -61,24 +41,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex">
-    <div id="mapContainer"></div>
-
-    <div>
-      <div>
-        <h3>Equipment Information</h3>
-        <p>Equipment Type: {{ equipmentModel?.name }}</p>
-        <p>Equipment Name: {{ equipmentType?.name }}</p>
-      </div>
-
-      <div v-if="equipmentStateList.length" class="flex flex-col p-4 w-full">
-        <div class="flex items-center w-full justify-between" v-for="(item, index) in equipmentStateList">
-          <p class="text-white" :key="index">{{ item?.name }} </p>
-          <span :class="`h-4 w-4 bg-[${ item?.color }] rounded-full`"></span>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div id="mapContainer"></div>
 </template>
 
 <style scoped>
