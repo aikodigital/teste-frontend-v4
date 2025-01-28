87d81cbad93d8d9521dfdaab7334beb09013beef
jaeder <zxb360@gmail.com>
2025-01-28 13:16:49 -0300

    falta filtra os estados

--- src/components/markingMap/MarkingMap.tsx
+++ src/components/markingMap/MarkingMap.tsx
@@ -31,26 +31,26 @@ export default function MarkingMap() {
   const { positions } = useContext(position); // equiID, position {data, lat, lon}
   const { stateEquipment } = useContext(estadoEquipamento)
   const { worked } = useContext(horasTrabalhadas)
-  const {equipmentHistory } = useContext(historicoEquipamento)
+  const {equipmentHistory } = useContext(historicoEquipamento) // get date => idState === estadoOpera & color
   
-  const [hoverInfo, setHoverInfo] = useState<string>("CA-0001");
+  const [hoverInfo, setHoverInfo] = useState();  // Aqui tem q ser Objeto ou array
   // const [filterPosition, setFilterPosition] = useState<[]>([])
-  // console.log(filterPosition);
+  // console.log(hoverInfo);
   
   // console.log('Tipo de estado', stateEquipment);
   // console.log('MODELO', modelo);  
   // console.log('historico equipamento', equipmentHistory);
+
   const filterModelo = modelo.find((model) => model.name === equipamentoId.modelEquipment)
   const filterPosition = positions.filter((position) => position.equipmentId === filterModelo?.id)
     .map((position) => {
       const positionFilter = position.positions.filter((position) => position.date === equipamentoId.datePosition)
-      return {
-        ...position,
-        positions: positionFilter
-      }
-    })
-  console.log(filterPosition);
-  
+          return {
+            ...position,
+            positions: positionFilter,
+          }
+      })
+      
   return isLoaded ? (
     <>
       <Header />
@@ -64,16 +64,30 @@ export default function MarkingMap() {
             key={index}
             // position={center}
             position={{
-              lat: position.lat, 
+                lat: position.lat, 
                 lng: position.lon,
               }}
-              
-              onMouseOver={() => setHoverInfo("Esperndo")}
-              onMouseOut={() => setHoverInfo(equipamentoId.modelEquipment)}
-              // onClick={() => setHoverInfo("click")}
+              // onMouseOver={() => {
+              //   const v = equipmentHistory.filter((e) => e.equipmentId === filterModelo?.id)
+              //   console.log(v);
+                
+              // }}
+              // onMouseOut={() => console.log("out")}
+              onClick={() => {
+                console.log(equipmentHistory.filter((eq) => eq.equipmentId === filterModelo?.id))
+                // .map((i) => {
+                //   // if (i.states[0].date !== equipamentoId.datePosition) 
+                //   return i.states.filter((a) => a.date === filterPosition[0].positions[0].date)
+                // })
+                // console.log(c);
+                
+                // const ve = stateEquipment.filter((w) => w.id === c[0][0].equipmentStateId)
+                // console.log(ve);
+              }}
+                      
               options={{
                 label: {
-                  text: hoverInfo === "EEE" ? hoverInfo : equipamentoId.modelEquipment,
+                  text: equipamentoId.datePosition,
                   className:'label-marker',
                 }
               }}
